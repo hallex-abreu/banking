56e3dc89298cf094b3a45557859333220423959e
Hallex Abreu <contato.hallexabreu@gmail.com>
2022-03-26 18:25:53 -0300

    class 02: JSON Encoding

--- main.go
+++ main.go
@@ -1,19 +1,45 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
+
+type Customer struct {
+	Name    string `json:"name"`
+	City    string `json:"city"`
+	Zipcode string `json:"zip_code"`
+}
 
 func main() {
 
 	//define routes
 	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getAllCustomers)
 
-	//start server
+	//starting server
 	http.ListenAndServe("localhost:8080", nil)
 }
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello World!!!")
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{
+			Name:    "Larah",
+			City:    "Eusébio",
+			Zipcode: "61760000",
+		},
+		{
+			Name:    "Hallex",
+			City:    "Eusébio",
+			Zipcode: "61760000",
+		},
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(customers)
 }
